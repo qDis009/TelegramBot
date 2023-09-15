0edd96071c7017c055f33349e7b6ac6aa1a49c16
Hamzin Daulet <qdis009@gmail.com>
2023-09-15 17:30:49 +0600

    config

---
+++ src/main/java/kz/qBots/fisrtBot/FirstBotApplication.java
@@ -0,0 +1,13 @@
+package kz.qBots.fisrtBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kz/qBots/fisrtBot/service/TelegramBot.java
@@ -0,0 +1,51 @@
+package kz.qBots.fisrtBot.service;
+
+import kz.qBots.fisrtBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig botConfig;
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig=botConfig;
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage()&&update.getMessage().hasText()){
+            String messageText=update.getMessage().getText();
+            long chatId=update.getMessage().getChatId();
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId,update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId,"Sorry, command was not realize");
+            }
+        }
+    }
+    private void startCommandReceived(long chatId,String firstName){
+        String answer="Hi, "+firstName+", nice to meet you!";
+        sendMessage(chatId,answer);
+    }
+    private void sendMessage(long chatId,String textToSend){
+        SendMessage message=new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        }catch (TelegramApiException e){
+
+        }
+    }
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+}
